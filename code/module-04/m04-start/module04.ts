d6afc5fcfc0446938c789fb907e4800f21999f96
DarioAlessioR <100212928+DarioAlessioR@users.noreply.github.com>
2023-12-28 11:33:13 +0100

    update module04.ts

--- code/module-04/m04-start/module04.ts
+++ code/module-04/m04-start/module04.ts
@@ -3,14 +3,14 @@
 
 /*  EXERCISE 1
     TODO: Declare a new function type for the sortDescending and sortAscending functions. */
-    
+    type compareFunctionType = (a: number, b:number) => number;
 
 /*  TODO: Convert the sortDescending and sortAscending functions to arrow 
     functions. */
 
 /*  sortDescending is a comparison function that tells the sort method how to sort 
     numbers in descending order */
-function sortDescending(a, b) {
+let sortDescending: compareFunctionType = (a, b) => {
 if (a > b) {
     return -1;
 } else if (b > a) {
@@ -22,7 +22,7 @@ if (a > b) {
 
 /*  sortDescending is a comparison function that tells the sort method how to sort 
     numbers in ascending order. */
-function sortAscending(a, b) {
+let sortAscending: compareFunctionType = (a, b) => {
 if (a > b) {
     return 1;
 } else if (b > a) {
@@ -38,9 +38,9 @@ if (a > b) {
 
 /*  TODO: Update the BuildArray function. */
 
-function buildArray(items, sortOrder) {
-    let randomNumbers = [];
-    let nextNumber;
+function buildArray(items: number, sortOrder: 'ascending' | 'descending'): number[] {
+    let randomNumbers: number[] = [];
+    let nextNumber: number;
     for (let counter = 0; counter < items; counter++) {
         nextNumber = Math.ceil(Math.random() * (100 - 1));
         if (randomNumbers.indexOf(nextNumber) === -1) {
@@ -58,6 +58,9 @@ function buildArray(items, sortOrder) {
 
 let myArray1 = buildArray(12, 'ascending');
 let myArray2 = buildArray(8, 'descending');
+
+console.log(myArray1);
+console.log(myArray2);
 
 /*  EXERCISE 2
     TODO: Update the LoanCalculator function. */
